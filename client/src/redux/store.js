5b91348fef238395da32475e03f9b7d7a73e525a
Mohammadu Amhar <mamamhar254@gmail.com>
2024-03-16 19:21:14 +0530

    and dark and light mood

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,13 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
 import { persistReducer } from "redux-persist";
+import themeReducer from "./theme/themeSlice.js";
 import storage from "redux-persist/lib/storage";
 import { persistStore } from "redux-persist";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 const persistConfig = {
