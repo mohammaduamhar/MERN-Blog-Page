0230cde40ce4da047a9131aa44d7278ba188923f
mohammaduamhar <mamamhar254@gmail.com>
2024-08-10 16:04:55 +0530

    set host as true and port to 80

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: true,  // Listen on all IP addresses, including public ones
+    port: 80,    // Use port 80 for the server
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
-       
         secure: false,
       },
     },
